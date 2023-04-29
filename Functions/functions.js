47a8fb09573801bdb93937326401d47c37d8f40f
nishantp15 <prajapati.nishant@outlook.com>
2023-04-29 17:17:25 +0530

    functions

---
+++ Functions/functions.js
@@ -0,0 +1,23 @@
+// function is reusable piece of code
+
+// Declaration
+
+function nameOfFunction(){
+    console.log("This is a function")
+}
+
+// function call or invocation
+nameOfFunction();
+
+// function expression :when function is taken as variable or when function is 
+// used as variable 
+
+let fun = function(){// also called anonymous function (un-named)
+    console.log("function expression");
+}
+
+// calling function expression
+
+fun();
+
+
